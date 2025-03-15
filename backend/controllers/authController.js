ac7069ee852554118114e7493bcb8fc27a91afc5
Kamran Mohammed <kamranabdulbasith@gmail.com>
2025-03-15 23:53:31 +0530

    idk2

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -19,12 +19,14 @@ const createAndSendToken = (user, statusCode, res) => {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
+    sameSite: "None",
     httpOnly: true,
   };
 
   // Set 'secure' only in production (so it works on localhost)
   if (process.env.NODE_ENV === "production") {
     cookieOptions.secure = true; // Cookies work only over HTTPS
+    console.log("secure set to true");
   } else {
     cookieOptions.secure = false; // Allow HTTP for localhost development
   }
